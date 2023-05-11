28d498e589a838cc9fc3c06cc40f5b590c112ef2
julia_celarscaia <julia.celarscaia@gmail.com>
2023-05-11 08:00:39 -0700

    decoration

--- source/js/app.js
+++
@@ -1,9 +0,0 @@
-const nav = document.querySelector('.nav');
-const navButton = document.querySelector('.nav-button');
-
-nav.classList.add('nav--with-js');
-nav.classList.add('nav--closed');
-
-navButton.addEventListener('click', function() {
-    nav.classList.toggle('nav--closed');
-});
